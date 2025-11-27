0747496fa2ce670f6fc80602cc05c3c36b84d5a6
BruceHu117 <135595468+BruceHu117@users.noreply.github.com>
2025-11-27 10:22:21 +0100

    debug

--- server/src/server.py
+++ server/src/server.py
@@ -915,8 +915,9 @@ def create_app():
 
 
 # WSGI entrypoint
-app = create_app()
+# app = create_app()
 
 if __name__ == "__main__":
+    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
